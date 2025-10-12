33094b0cf8e11d8d5cbaadcebb70461d8db84af3
durgasahusahu <durgasahusahu1@gmail.com>
2025-10-12 10:23:48 +0530

    first commit

---
+++ L1/app.js
@@ -0,0 +1,7 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var n1 = 23;
+var n2 = 33;
+var ans = add(n1, n2);
+console.log("ans ", ans);
---
+++ L1/app.ts
@@ -0,0 +1,9 @@
+// function add (num1,num2){
+//     return num1 + num2
+// }
+
+// const n1 = 23;
+// const n2 = 33;
+
+// const ans = add(n1,n2);
+// console.log("ans ",ans); 
